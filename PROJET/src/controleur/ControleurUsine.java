18faa51857d3d0a65fab8458c208c8be08045761
tovarich <calmels.dimitri@gmail.com>
2019-01-23 10:06:00 +0100

    modif osef

--- PROJET/src/controleur/ControleurUsine.java
+++ PROJET/src/controleur/ControleurUsine.java
@@ -9,8 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
 import static java.nio.file.StandardOpenOption.APPEND;
 
 import modele.ChaineDeProduction;
